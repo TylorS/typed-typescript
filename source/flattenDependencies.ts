733db2de6cad53fc24b661ef9cf712917f8dfe0a
Tylor Steinberger <tylor@tryfynd.com>
2018-11-22 07:44:59 -0500

    feat: improve flatten dependencies to put dependencies into concatenation order

--- source/flattenDependencies.ts
+++ source/flattenDependencies.ts
@@ -1,15 +1,63 @@
-import { chain } from '@typed/list'
 import { Dependency, DependencyTree } from './types'
 
-export function flattenDependencies({ type, path, dependencies }: DependencyTree): Dependency[] {
-  const names: Dependency[] = [{ type, path }]
-  const hasBeenSeen = (path: string) => names.findIndex(x => x.path === path) > -1
+export function flattenDependencies(tree: DependencyTree): Dependency[] {
+  return resolveDependencyOrder(treeToMap(tree), tree)
+}
 
-  for (const dependency of chain(flattenDependencies, dependencies)) {
-    if (!hasBeenSeen(dependency.path)) {
-      names.push(dependency)
+function treeToMap(dependencyTree: DependencyTree): DepMap {
+  const map: DepMap = new Map()
+  const depsToProcess = [dependencyTree]
+
+  while (depsToProcess.length > 0) {
+    const tree = depsToProcess.shift() as DependencyTree
+    const list = map.get(tree) || []
+
+    for (const dependency of tree.dependencies) {
+      depsToProcess.push(dependency)
+      list.push(dependency)
     }
+
+    map.set(tree, list)
   }
 
-  return names
+  return map
+}
+
+type DepMap = Map<DependencyTree, DependencyTree[]>
+
+function resolveDependencyOrder(depMap: DepMap, dependency: DependencyTree): Dependency[] {
+  const result: Dependency[] = []
+
+  resolveSpecific(depMap, result, dependency, [dependency.path])
+
+  return result
+}
+
+function resolveSpecific(
+  depMap: DepMap,
+  result: Dependency[],
+  dependency: DependencyTree,
+  filesProcessed: string[],
+) {
+  const path = dependency.path
+  if (filesProcessed.indexOf(path) !== filesProcessed.lastIndexOf(path)) {
+    throw new Error('Circular dependency found: ' + filesProcessed.join(' > '))
+  }
+
+  const deps = depMap.get(dependency)
+
+  if (deps) {
+    deps.forEach((dep: DependencyTree) =>
+      resolveSpecific(depMap, result, dep, filesProcessed.concat(dep.path)),
+    )
+  }
+
+  const index = result.findIndex(x => x.path === path)
+
+  if (index === -1) {
+    result.push(dependency)
+    depMap.delete(dependency)
+  }
+
+  return result
 }
