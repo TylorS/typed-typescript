5a48ff0c38b4eb263946198ad3bb7c7c2e624883
Tylor Steinberger <tylor@tryfynd.com>
2018-11-29 18:54:42 -0500

    feat: cleanup dependency tree when a file is removed

--- source/watchSourceFiles/DependencyManager.ts
+++ source/watchSourceFiles/DependencyManager.ts
@@ -50,10 +50,15 @@ export function createDependencyManager({
     }
   }
 
-  // TODO: This should probably clean up dependencies info if not
-  // dependended on by other files
+  function updateFile(file: string) {
+    fileVersionManager.updateFile(file)
+    addFile(file)
+  }
+
   function unlinkFile(file: string) {
     const filePath = getPath(file)
+
+    getDependentsOf(filePath).forEach(updateFile)
 
     fileVersionManager.unlinkFile(filePath)
 
@@ -63,6 +68,14 @@ export function createDependencyManager({
 
     if (dependentMap[filePath]) {
       delete dependentMap[filePath]
+    }
+
+    if (dependentOf[filePath]) {
+      delete dependentOf[filePath]
+    }
+
+    if (dependentVersionMap[filePath]) {
+      delete dependentVersionMap[filePath]
     }
   }
 
@@ -121,13 +134,10 @@ export function createDependencyManager({
 
   return {
     addFile: (file: string) => {
-      addFile(file)
       fileVersionManager.addFile(file)
-    },
-    updateFile: (file: string) => {
       addFile(file)
-      fileVersionManager.updateFile(file)
     },
+    updateFile,
     unlinkFile,
     isDependentOf,
     getDependenciesOf,
